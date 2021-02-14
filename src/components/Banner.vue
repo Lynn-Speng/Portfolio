cf501ceb45e50a81ce8ccce3af7053e61d85d4fb
Lynn Speng <Lynn.Speng@outlook.com>
2021-02-14 15:26:52 +0800

    add responsive style

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -20,11 +20,11 @@ export default {
 <style>
 .banner {
   width: 100%;
-  height: 480px;
+  height: 600px;
   background-color: #1b1b1b;
   color: white;
   box-sizing: border-box;
-  padding: 100px 300px;
+  padding: 50px 500px;
   display: flex;
 }
 
@@ -56,7 +56,75 @@ export default {
   object-fit: fill;
 }
 
-.banner {
-  padding: 50px 500px;
+@media screen and (max-width: 1200px) {
+  .banner {
+    height: 320px;
+    padding: 30px 50px;
+  }
+
+  .title {
+    flex: 1;
+    padding: 10px 50px;
+  }
+
+  .illustration {
+    flex: 1;
+    /* display: none; */
+  }
+
+  .illustration img {
+    width: 313px;
+    height: 250px;
+    object-fit: fill;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .banner {
+    height: 240px;
+    padding: 10px 30px;
+  }
+
+  .title {
+    flex: 1;
+    padding: 10px 5px;
+  }
+
+  .illustration {
+    flex: 1;
+    display: none;
+  }
+
+  .illustration img {
+    width: 500px;
+    height: 400px;
+    object-fit: fill;
+  }
+}
+
+/* iPhone 5/SE */
+@media screen and (max-width: 320px) {
+  .banner {
+    height: 220px;
+    padding: 8px 15px;
+  }
+
+  .title {
+    flex: 1;
+    padding: 10px 5px;
+  }
+
+  .title h1 {
+    font-size: 30px;
+  }
+
+  .title h3 {
+    font-size: 22px;
+  }
+
+  .illustration {
+    flex: 1;
+    display: none;
+  }
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,11 +22,30 @@ export default {
 <style>
 .footer {
   padding: 10px 500px;
-  font-family: "Palatino Linotype";
 }
 .footer p {
+  font-family: "Palatino Linotype";
   text-align: center;
   text-decoration: blue;
   margin: 5px auto;
+}
+
+@media screen and (max-width: 1200px) {
+  .footer {
+    padding: 10px 50px;
+  }
+  .footer p {
+    margin: 5px auto;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .footer {
+    padding: 10px 20px;
+  }
+  .footer p {
+    font-size: 16px;
+    margin: 5px auto;
+  }
 }
 </style>
